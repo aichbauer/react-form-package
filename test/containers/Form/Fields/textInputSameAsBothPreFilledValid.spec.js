ecd07fff20d39d987f089329a6247bcd7951f0ef
Lukas Aichbauer <l.aichbauer@gmail.com>
2018-11-24 15:01:38 +0100

    Fix: initial state (#33)
    * Fix: sync state of sameAs on mount, add data to initialState
    
    * Test: add for new fix
    
    * Refactor: rm console.log
    
    * Test: rm unnecessary wrapper update

---
+++ test/containers/Form/Fields/textInputSameAsBothPreFilledValid.spec.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Adapter from 'enzyme-adapter-react-16';
+import { configure, mount } from 'enzyme';
+
+import {
+  Form,
+  Field,
+} from '../../../../src';
+
+configure({ adapter: new Adapter() });
+
+test('Render Form with two text inputs and pre filled value | sameAs | both should be valid', async () => {
+  const myComponent = (
+    <Form>
+      <Field type="text" sameAs="text2" id="text" required value="Test" />
+      <Field type="text" sameAs="text" id="text2" required value="Test" />
+    </Form>
+  );
+
+  const tree = renderer.create(myComponent).toJSON();
+  const wrapper = mount(myComponent);
+  const state = wrapper.state();
+
+  expect(state.data.text.value).toBe('Test');
+  expect(state.data.text.valid).toBe(true);
+  expect(state.data.text2.value).toBe('Test');
+  expect(state.data.text2.valid).toBe(true);
+
+  expect(tree).toMatchSnapshot();
+});
